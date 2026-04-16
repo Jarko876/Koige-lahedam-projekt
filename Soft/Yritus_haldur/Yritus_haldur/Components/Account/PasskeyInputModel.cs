a6d7c95a8c5a7ba0244e38624da4e3243fe8f42b
jarko <jakorv@taltech.ee>
2026-04-16 15:01:16 +0300

    Rakenduse lisamine

---
+++ Soft/Yritus_haldur/Yritus_haldur/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace Abc.Soft.Yritus_haldur.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
