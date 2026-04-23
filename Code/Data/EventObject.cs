37c06e9b8695796e4580cbd543768dcbc66f41e3
Sofia Viikant <sofiav@taltech.ee>
2026-04-23 15:42:42 +0300

    Lisasin EventObjectile ja Genrele propertyd

--- Code/Data/EventObject.cs
+++ Code/Data/EventObject.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Abc.Data
 {
-    internal class EventObject
+    public enum EventType
     {
+        Film,
+        Etendus
+    }
+    public enum AgeRating
+    {
+        G,
+        PG,
+        PG13,
+        R,
+        NC17
+    }
+    public class EventObject
+    {
+        public EventType Type { get; set; }
+        public AgeRating Rating { get; set; }
+        [Required] public string OriginalLanguage { get; set; } = "";
+        [Range(1, 1000)] public int DurationMinutes { get; set; }
+        [Required] public string Description { get; set; } = "";
+        public DateTime ReleaseDate { get; set; }
+
     }
 }
