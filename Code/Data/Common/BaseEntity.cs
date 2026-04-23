a943be18b99da3a96148a1679722ca97f6660169
jarko <jakorv@taltech.ee>
2026-04-23 16:20:47 +0300

    Datasse klasside lisamine, Shared lisamine, Datasse common lisamine

---
+++ Code/Data/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abc.Data.Common;
+
+public abstract class BaseEntity {
+    public virtual Guid Id { get; set; } = Guid.NewGuid();
+    [Timestamp] public virtual byte[] Timestamp { get; set; } = [];
+}
---
+++ Code/Data/Common/DetailedEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace Abc.Data.Common;
+
+public abstract class DetailedEntity : NamedEntity {
+    public virtual string Details { get; set; }
+}
