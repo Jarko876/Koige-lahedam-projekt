f789230f118905504ebef16f8083d9b07c63f5ce
Allan Saariste <allans@taltech.ee>
2026-04-25 12:41:36 +0300

    Lisasin klassid Ticket, Seat ja SeatGategory

---
+++ Code/Data/SeatGategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Data
+{
+    public class SeatCategory
+    {
+        //vastutab Allan
+    }
+}
---
+++ Code/Data/Ticket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Data
+{
+    public class Ticket
+    {
+        //vastutab Allan
+    }
+}
