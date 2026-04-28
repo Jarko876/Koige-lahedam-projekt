f99de1eb6a73476f1fc294d6a8daeb7fee105a72
elise <elise@EL>
2026-04-28 21:07:21 +0300

    Cart ja Payment klassid

---
+++ Code/Data/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Abc.Data.Common;
+
+namespace Abc.Data;
+
+public class Cart : BaseEntity {
+    
+    public int PersonId { get; set; }
+    //public Person Person { get; set; } - hiljem lisada?
+    public DateTime CreatedAt { get; set; }
+    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>(); //üks ostukorv võib sisaldada mitut piletit
+
+}
---
+++ Code/Data/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Data.Common;
+
+namespace Abc.Data;
+
+public class Payment : BaseEntity {
+    public Guid CartId { get; set; }
+    public Cart Cart { get; set; }
+    public decimal Amount { get; set; }
+    public string PaymentStatus { get; set; } //pending v paid
+    public DateTime PaymentDate { get; set; }
+
+}
